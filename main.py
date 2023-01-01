cc354a2ed7832661e4ff5ab0e7dc9fc916259992
Ronen Saviz <ronen@oversight.co.il>
2023-01-01 15:43:14 +0200

    main

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 1.A
+aList = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def creat_let_list():
+    for i in range(97, 123):
+        aList.append(chr(i))
+    print(aList)
+
+# 1.B
+def mod_chrs():
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    creat_let_list()
